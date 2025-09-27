a26797a0910e0fb477aea77175a4841a35aefd59
mongilyov <andrey1987697@gmail.com>
2025-09-27 13:13:51 +0300

    Added ftplugin/ directory for specific file settings

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,7 +1,6 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 vim.g.mapleader = " "
 vim.g.background = "light"
